29ec58157f7b755db9ce4104d27accf88f236a64
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2024-07-13 18:29:38 +0300

    small spelling/grammar mistakes fixed

--- frontend/src/Components/Newsletter/Newsletter.jsx
+++ frontend/src/Components/Newsletter/Newsletter.jsx
@@ -5,7 +5,7 @@ import './Newsletter.css'
 const Newsletter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclusive Offers On Your Email</h1>
+        <h1>Get Exclusive Offers in Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
         <div>
             <input type="email" placeholder='Your Email id' />
