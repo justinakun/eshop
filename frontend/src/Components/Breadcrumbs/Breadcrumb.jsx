adf991ec20afdafa270fabdb70a8c3984e8df331
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2024-07-13 20:31:04 +0300

    spelling mistake fixed

--- frontend/src/Components/Breadcrumbs/Breadcrumb.jsx
+++ frontend/src/Components/Breadcrumbs/Breadcrumb.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import './Breadcrum.css'
+import './Breadcrumb.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
-const Breadcrum = (props) => {
+const Breadcrumb = (props) => {
     const {product} = props;
   return (
-    <div className='breadcrum'>
+    <div className='breadcrumb'>
        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
     </div>
   )
 }
 
-export default Breadcrum
+export default Breadcrumb
